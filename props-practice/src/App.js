27b4311f811c3b3c5591292da2eb3779d732781c
muthukumar <anikumar2825@gmail.com>
2022-08-18 21:58:13 +0530

    add-file.

---
+++ props-practice/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import Header from './header/header';
+import Props from './content/props';
+
+function App() {
+  return(
+    <div>
+      <Header/>
+      <Props/>
+    </div>
+  )
+}
+
+export default App;
---
+++ props-practice/src/header/header.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="head">
+      <div>
+        <img src={require("../header/logo192.png")} />
+      </div>
+      <div className="h-left">
+        <div className="ip">
+          <input />
+          <i className="fa fa-search" style={{ position: "absolute" ,right:'335px',top:'33px',color:'white',fontSize:'20px'}}>
+        
+          </i>
+        </div>
+
+        <i
+          className="fa fa-shopping-cart"
+          style={{ margin: "0px 20px", fontSize: "30px", color: "white" }}
+        ></i>
+
+        <div className="cart">M</div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
